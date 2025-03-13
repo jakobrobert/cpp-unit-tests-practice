b3fd580d68a1a28e6a7369dc7597a0fdee7371fc
jakobrobert <jakob-robert@hotmail.de>
2025-03-13 15:37:01 +0100

    Add test FactorialOfOne (already passes)

--- test/TestMathFunctions.cpp
+++ test/TestMathFunctions.cpp
@@ -6,3 +6,8 @@ TEST(TestMathFunctions, FactorialOfZero)
 {
     EXPECT_EQ(1, MathFunctions::Factorial(0));
 }
+
+TEST(TestMathFunctions, FactorialOfOne)
+{
+    EXPECT_EQ(1, MathFunctions::Factorial(1));
+}
