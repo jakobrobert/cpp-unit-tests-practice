e3c1024e6f852bf2352714de6579a371c0aedf1d
jakobrobert <jakob-robert@hotmail.de>
2025-03-13 16:25:34 +0100

    Extend impl to make test FactorialOfNegativeNumber pass

--- lib/MathFunctions.cpp
+++ lib/MathFunctions.cpp
@@ -1,7 +1,12 @@
 #include "MathFunctions.h"
 
+#include <stdexcept>
+
 int MathFunctions::Factorial(int n)
 {
+    if (n < 0)
+        throw std::invalid_argument("Factorial is not defined for negative numbers");
+
     if (n < 2)
         return 1;
 
