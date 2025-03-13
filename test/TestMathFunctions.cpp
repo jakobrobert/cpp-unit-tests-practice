547887ed8ce9e6e860a993f7df23bcd85d567cf0
jakobrobert <jakob-robert@hotmail.de>
2025-03-13 15:51:55 +0100

    Add failing test FactorialOfThree

--- test/TestMathFunctions.cpp
+++ test/TestMathFunctions.cpp
@@ -2,6 +2,8 @@
 
 #include "MathFunctions.h"
 
+// TODO Test exception for negative input
+
 TEST(TestMathFunctions, FactorialOfZero)
 {
     EXPECT_EQ(1, MathFunctions::Factorial(0));
@@ -15,4 +17,9 @@ TEST(TestMathFunctions, FactorialOfOne)
 TEST(TestMathFunctions, FactorialOfTwo)
 {
     EXPECT_EQ(2, MathFunctions::Factorial(2));
+}
+
+TEST(TestMathFunctions, FactorialOfThree)
+{
+    EXPECT_EQ(6, MathFunctions::Factorial(3));
 }
