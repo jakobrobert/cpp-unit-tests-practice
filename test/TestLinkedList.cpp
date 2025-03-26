36aaba56c879f80d39e2a8170d79bc586f85f4ae
jakobrobert <jakob-robert@hotmail.de>
2025-03-26 16:04:55 +0100

    Extend test Empty to test At(0)

--- test/TestLinkedList.cpp
+++ test/TestLinkedList.cpp
@@ -7,7 +7,7 @@ TEST(TestLinkedList, Empty)
     LinkedList<int> list;
     EXPECT_TRUE(list.IsEmpty());
     EXPECT_EQ(0, list.Size());
-    // TODO test At(0) -> exception
+    EXPECT_THROW(list.At(0), std::invalid_argument);
 }
 
 // TODO Test IsEmpty returns false for at least one element (need Add functionality for that)
