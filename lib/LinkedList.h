ee8e832278f329e608b3c2bb23204ba912c55db8
jakobrobert <jakob-robert@hotmail.de>
2025-03-26 15:47:53 +0100

    Add Size(), add assertion in test

--- lib/LinkedList.h
+++ lib/LinkedList.h
@@ -4,5 +4,9 @@ template <typename T>
 class LinkedList
 {
 public:
+    void Add(T value) { }
+
     bool IsEmpty() const { return true; }
+    size_t Size() const { return 0; }
+    // TODO method At(size_t index)
 };
--- test/TestLinkedList.cpp
+++ test/TestLinkedList.cpp
@@ -6,6 +6,7 @@ TEST(TestLinkedList, IsEmptyReturnsTrueForEmptyList)
 {
     LinkedList<int> list;
     EXPECT_TRUE(list.IsEmpty());
+    EXPECT_EQ(0, list.Size());
 }
 
 // TODO Test IsEmpty returns false for at least one element (need Add functionality for that)
