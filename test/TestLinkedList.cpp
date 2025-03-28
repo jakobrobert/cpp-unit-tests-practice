6c74ad11561634d833948eef4b1eea01e07a483c
jakobrobert <jakob-robert@hotmail.de>
2025-03-28 15:20:53 +0100

    Add failing test RemoveMultipleElements

--- test/TestLinkedList.cpp
+++ test/TestLinkedList.cpp
@@ -50,5 +50,26 @@ TEST(TestLinkedList, AddMultipleElements)
     EXPECT_THROW(list.At(3), std::out_of_range);
 }
 
-// TODO Test RemoveMultipleElements
+TEST(TestLinkedList, RemoveMultipleElements)
+{
+    LinkedList<int> list;
 
+    list.Add(3);
+    list.Add(1);
+    list.Add(2);
+    EXPECT_FALSE(list.IsEmpty());
+    EXPECT_EQ(3, list.Size());
+    EXPECT_EQ(3, list.At(0));
+    EXPECT_EQ(1, list.At(1));
+    EXPECT_EQ(2, list.At(2));
+    EXPECT_THROW(list.At(3), std::out_of_range);
+
+    list.RemoveFirst();
+    list.RemoveFirst();
+    EXPECT_FALSE(list.IsEmpty());
+    EXPECT_EQ(1, list.Size());
+    EXPECT_EQ(3, list.At(0));
+    EXPECT_THROW(list.At(1), std::out_of_range);
+}
+
+// TODO RemoveFromEmptyList -> expect out_of_range
