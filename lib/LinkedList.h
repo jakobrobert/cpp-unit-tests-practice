74ec42055ffbf37029492baa15fa3b4eeec0cf98
jakobrobert <jakob-robert@hotmail.de>
2025-03-29 10:36:20 +0100

    #6 Add LinkedList & tests (#8) * Add LinkedList class & IsEmpty method with dummy impl
    * Add failing test IsEmptyReturnsTrueForEmptyList
    
    * Make test IsEmptyReturnsTrueForEmptyList pass
    
    * Add Size(), add assertion in test
    
    * Add failing test AddOneElement & add method At()
    
    * Extend test Empty to test At(0)
    
    * Make test Empty pass again by extending At()
    
    * Make test AddOneElement pass
    
    * Refactor Size() -> Use IsEmpty() for consistency
    
    * Add TODOs for tests
    
    * Refactor: Inline IsEmpty() for better readability
    
    * Optimize: use const ref
    
    * Add failing test RemoveOneElement
    
    * Make test  void RemoveOneElement pass
    
    * Add failing test AddMultipleElements
    
    * Make test AddMultipleElements pass
    
    * Fix regression in test RemoveOneElement
    
    * Adjust tests to expect std::out_of_range instead of std::invalid_argument, this makes them fail as expected
    
    * Adjust impl to make tests pass again
    
    * Extend tests with checks for out_of_range
    
    * Refactor: Rename RemoveLast() to RemoveFirst()
    
    * Add failing test RemoveMultipleElements
    
    * Optimize: Adjust At() to return const ref
    
    * Refactor: Rename variable
    
    * Make test RemoveMultipleElements pass by extending impl & fixing test
    
    * Add failing test TryToRemoveFromEmptyList
    
    * Remove obsolete comment
    
    * Make test TryToRemoveFromEmptyList pass
    
    * Add failing test Clear
    
    * Make test Clear pass
    
    * Add destructor -> it delegates to Clear()

---
+++ lib/LinkedList.h
@@ -0,0 +1,91 @@
+#pragma once
+
+template <typename T>
+class LinkedList
+{
+private:
+    struct Node
+    {
+        Node(const T& value)
+            : value(value), next(nullptr)
+        {}
+
+        T value;
+        Node* next;
+    };
+
+public:
+    ~LinkedList()
+    {
+        Clear();
+    }
+
+    void Add(const T& value)
+    {
+        Node* newNode = new Node(value);
+
+        if (head == nullptr)
+        {
+            head = last = newNode;
+        }
+        else
+        {
+            last->next = newNode;
+            last = newNode;
+        }
+
+        size++;
+    }
+
+    void RemoveFirst()
+    {
+        if (head == nullptr)
+            throw std::out_of_range("List is empty");
+
+        Node* oldHead = head;
+        head = head->next;
+        delete oldHead;
+        size--;
+    }
+
+    void Clear()
+    {
+        while (!IsEmpty())
+            RemoveFirst();
+    }
+
+    bool IsEmpty() const
+    {
+        return head == nullptr;
+    }
+
+    size_t Size() const
+    {
+        return size;
+    }
+    
+    const T& At(size_t index) const
+    {
+        if (head == nullptr)
+            throw std::out_of_range("List is empty");
+
+        size_t currentIndex = 0;
+        Node* currentNode = head;
+        
+        while (currentNode != nullptr)
+        {
+            if (currentIndex == index)
+                return currentNode->value;
+
+            currentNode = currentNode->next;
+            currentIndex++;
+        }
+
+        throw std::out_of_range("Index is too large");
+    }
+
+private:
+    Node* head = nullptr;
+    Node* last = nullptr;
+    size_t size = 0;
+};
---
+++ test/TestLinkedList.cpp
@@ -0,0 +1,99 @@
+#include <gtest/gtest.h>
+
+#include "LinkedList.h"
+
+TEST(TestLinkedList, Empty)
+{
+    LinkedList<int> list;
+    EXPECT_TRUE(list.IsEmpty());
+    EXPECT_EQ(0, list.Size());
+    EXPECT_THROW(list.At(0), std::out_of_range);
+}
+
+TEST(TestLinkedList, AddOneElement)
+{
+    LinkedList<int> list;
+    list.Add(42);
+    EXPECT_FALSE(list.IsEmpty());
+    EXPECT_EQ(1, list.Size());
+    EXPECT_EQ(42, list.At(0));
+    EXPECT_THROW(list.At(1), std::out_of_range);
+}
+
+TEST(TestLinkedList, RemoveOneElement)
+{
+    LinkedList<int> list;
+
+    list.Add(42);
+    EXPECT_FALSE(list.IsEmpty());
+    EXPECT_EQ(1, list.Size());
+    EXPECT_EQ(42, list.At(0));
+    EXPECT_THROW(list.At(1), std::out_of_range);
+
+    list.RemoveFirst();
+    EXPECT_TRUE(list.IsEmpty());
+    EXPECT_EQ(0, list.Size());
+    EXPECT_THROW(list.At(0), std::out_of_range);
+}
+
+TEST(TestLinkedList, AddMultipleElements)
+{
+    LinkedList<int> list;
+    list.Add(3);
+    list.Add(1);
+    list.Add(2);
+    EXPECT_FALSE(list.IsEmpty());
+    EXPECT_EQ(3, list.Size());
+    EXPECT_EQ(3, list.At(0));
+    EXPECT_EQ(1, list.At(1));
+    EXPECT_EQ(2, list.At(2));
+    EXPECT_THROW(list.At(3), std::out_of_range);
+}
+
+TEST(TestLinkedList, RemoveMultipleElements)
+{
+    LinkedList<int> list;
+
+    list.Add(3);
+    list.Add(1);
+    list.Add(2);
+    EXPECT_FALSE(list.IsEmpty());
+    EXPECT_EQ(3, list.Size());
+    EXPECT_EQ(3, list.At(0));
+    EXPECT_EQ(1, list.At(1));
+    EXPECT_EQ(2, list.At(2));
+    EXPECT_THROW(list.At(3), std::out_of_range);
+
+    list.RemoveFirst();
+    list.RemoveFirst();
+    EXPECT_FALSE(list.IsEmpty());
+    EXPECT_EQ(1, list.Size());
+    EXPECT_EQ(2, list.At(0));
+    EXPECT_THROW(list.At(1), std::out_of_range);
+}
+
+TEST(TestLinkedList, TryToRemoveFromEmptyList)
+{
+    LinkedList<int> list;
+    EXPECT_THROW(list.RemoveFirst(), std::out_of_range);
+}
+
+TEST(TestLinkedList, Clear)
+{
+    LinkedList<int> list;
+
+    list.Add(3);
+    list.Add(1);
+    list.Add(2);
+    EXPECT_FALSE(list.IsEmpty());
+    EXPECT_EQ(3, list.Size());
+    EXPECT_EQ(3, list.At(0));
+    EXPECT_EQ(1, list.At(1));
+    EXPECT_EQ(2, list.At(2));
+    EXPECT_THROW(list.At(3), std::out_of_range);
+
+    list.Clear();
+    EXPECT_TRUE(list.IsEmpty());
+    EXPECT_EQ(0, list.Size());
+    EXPECT_THROW(list.At(0), std::out_of_range);
+}
