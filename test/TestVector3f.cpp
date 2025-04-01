022924bc655d0332a7f465f6029d1d15be7749fe
jakobrobert <jakob-robert@hotmail.de>
2025-04-01 11:11:23 +0200

    Simplify names in test

--- test/TestVector3f.cpp
+++ test/TestVector3f.cpp
@@ -6,32 +6,32 @@ constexpr float FLOAT_TOLERANCE = 1e-6f;
 
 TEST(TestVector3f, DefaultConstructor)
 {
-    Vector3f vector;
-    EXPECT_EQ(0.f, vector.x);
-    EXPECT_EQ(0.f, vector.y);
-    EXPECT_EQ(0.f, vector.z);
+    Vector3f v;
+    EXPECT_EQ(0.f, v.x);
+    EXPECT_EQ(0.f, v.y);
+    EXPECT_EQ(0.f, v.z);
 }
 
 TEST(TestVector3f, ParameterizedConstructor)
 {
-    Vector3f vector(-1.0f, 1e6f, 1e-6f);
-    EXPECT_NEAR(-1.0f, vector.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(1e6f, vector.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(1e-6f, vector.z, FLOAT_TOLERANCE);
+    Vector3f v(-1.0f, 1e6f, 1e-6f);
+    EXPECT_NEAR(-1.0f, v.x, FLOAT_TOLERANCE);
+    EXPECT_NEAR(1e6f, v.y, FLOAT_TOLERANCE);
+    EXPECT_NEAR(1e-6f, v.z, FLOAT_TOLERANCE);
 }
 
 TEST(TestVector3f, OperatorEqualityVectorsAreEqual)
 {
-    Vector3f vec1(1.f, 2.f, 3.f);
-    Vector3f vec2(1.f, 2.f, 3.f);
-    EXPECT_TRUE(vec1 == vec2);
-    EXPECT_FALSE(vec1 != vec2);
+    Vector3f v1(1.f, 2.f, 3.f);
+    Vector3f v2(1.f, 2.f, 3.f);
+    EXPECT_TRUE(v1 == v2);
+    EXPECT_FALSE(v1 != v2);
 }
 
 TEST(TestVector3f, OperatorEqualityVectorsAreNotEqual)
 {
-    Vector3f vec1(1.f, 2.f, 3.f);
-    Vector3f vec2(1.f, 2.f, 4.f);
-    EXPECT_FALSE(vec1 == vec2);
-    EXPECT_TRUE(vec1 != vec2);
+    Vector3f v1(1.f, 2.f, 3.f);
+    Vector3f v2(1.f, 2.f, 4.f);
+    EXPECT_FALSE(v1 == v2);
+    EXPECT_TRUE(v1 != v2);
 }
