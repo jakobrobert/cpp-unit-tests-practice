fcbb81159095fd9d690f8bf87893b7522f499c81
jakobrobert <jakob-robert@hotmail.de>
2025-04-17 16:57:01 +0200

    Refactor: In tests, extract method ExpectVector3Near to avoid duplication

--- test/TestVector3.cpp
+++ test/TestVector3.cpp
@@ -4,6 +4,13 @@
 
 constexpr float FLOAT_TOLERANCE = 1e-6f;
 
+void ExpectVector3Near(const Vector3& expected, const Vector3& actual, float tolerance)
+{
+    EXPECT_NEAR(expected.x, actual.x, tolerance);
+    EXPECT_NEAR(expected.y, actual.y, tolerance);
+    EXPECT_NEAR(expected.z, actual.z, tolerance);
+}
+
 TEST(TestVector3, DefaultConstructor)
 {
     Vector3 v;
@@ -15,6 +22,7 @@ TEST(TestVector3, DefaultConstructor)
 TEST(TestVector3, ParameterizedConstructor)
 {
     Vector3 v(-1.0f, 1e6f, 1e-6f);
+    // Note: Do not use ExpectVector3Near here. By using the constructor to create the expected vector, this would create a circular dependency and make the test meaningless.
     EXPECT_NEAR(-1.0f, v.x, FLOAT_TOLERANCE);
     EXPECT_NEAR(1e6f, v.y, FLOAT_TOLERANCE);
     EXPECT_NEAR(1e-6f, v.z, FLOAT_TOLERANCE);
@@ -43,9 +51,8 @@ TEST(TestVector3, OperatorPlusEquals)
 
     v1 += v2;
 
-    EXPECT_NEAR(5.5f, v1.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(7.7f, v1.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(9.9f, v1.z, FLOAT_TOLERANCE);
+    Vector3 expected(5.5f, 7.7f, 9.9f);
+    ExpectVector3Near(expected, v1, FLOAT_TOLERANCE);
 }
 
 TEST(TestVector3, OperatorPlus)
@@ -53,11 +60,10 @@ TEST(TestVector3, OperatorPlus)
     Vector3 v1(1.1f, 2.2f, 3.3f);
     Vector3 v2(4.4f, 5.5f, 6.6f);
 
-    Vector3 v3 = v1 + v2;
+    Vector3 actual = v1 + v2;
 
-    EXPECT_NEAR(5.5f, v3.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(7.7f, v3.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(9.9f, v3.z, FLOAT_TOLERANCE);
+    Vector3 expected(5.5f, 7.7f, 9.9f);
+    ExpectVector3Near(expected, actual, FLOAT_TOLERANCE);
 }
 
 TEST(TestVector3, OperatorMinusEquals)
@@ -67,9 +73,8 @@ TEST(TestVector3, OperatorMinusEquals)
 
     v1 -= v2;
 
-    EXPECT_NEAR(1.1f, v1.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(2.2f, v1.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(3.3f, v1.z, FLOAT_TOLERANCE);
+    Vector3 expected(1.1f, 2.2f, 3.3f);
+    ExpectVector3Near(expected, v1, FLOAT_TOLERANCE);
 }
 
 TEST(TestVector3, OperatorMinus)
@@ -77,11 +82,10 @@ TEST(TestVector3, OperatorMinus)
     Vector3 v1(5.5f, 7.7f, 9.9f);
     Vector3 v2(4.4f, 5.5f, 6.6f);
 
-    Vector3 v3 = v1 - v2;
+    Vector3 actual = v1 - v2;
 
-    EXPECT_NEAR(1.1f, v3.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(2.2f, v3.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(3.3f, v3.z, FLOAT_TOLERANCE);
+    Vector3 expected(1.1f, 2.2f, 3.3f);
+    ExpectVector3Near(expected, actual, FLOAT_TOLERANCE);
 }
 
 TEST(TestVector3, OperatorMultiplyEquals)
@@ -90,20 +94,18 @@ TEST(TestVector3, OperatorMultiplyEquals)
 
     v1 *= 42.42f;
     
-    EXPECT_NEAR(46.662f, v1.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(93.324f, v1.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(139.986f, v1.z, FLOAT_TOLERANCE);
+    Vector3 expected(46.662f, 93.324f, 139.986f);
+    ExpectVector3Near(expected, v1, FLOAT_TOLERANCE);
 }
 
 TEST(TestVector3, OperatorMultiply)
 {
     Vector3 v1(1.1f, 2.2f, 3.3f);
 
-    Vector3 v2 = v1 * 42.42f;
+    Vector3 actual = v1 * 42.42f;
     
-    EXPECT_NEAR(46.662f, v2.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(93.324f, v2.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(139.986f, v2.z, FLOAT_TOLERANCE);
+    Vector3 expected(46.662f, 93.324f, 139.986f);
+    ExpectVector3Near(expected, actual, FLOAT_TOLERANCE);
 }
 
 TEST(TestVector3, OperatorDivideEquals)
@@ -111,21 +113,19 @@ TEST(TestVector3, OperatorDivideEquals)
     Vector3 v1(46.662f, 93.324f, 139.986f);
 
     v1 /= 42.42f;
-    
-    EXPECT_NEAR(1.1f, v1.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(2.2f, v1.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(3.3f, v1.z, FLOAT_TOLERANCE);
+
+    Vector3 expected(1.1f, 2.2f, 3.3f);
+    ExpectVector3Near(expected, v1, FLOAT_TOLERANCE);
 }
 
 TEST(TestVector3, OperatorDivide)
 {
     Vector3 v1(46.662f, 93.324f, 139.986f);
 
-    Vector3 v2 = v1 / 42.42f;
+    Vector3 actual = v1 / 42.42f;
     
-    EXPECT_NEAR(1.1f, v2.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(2.2f, v2.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(3.3f, v2.z, FLOAT_TOLERANCE);
+    Vector3 expected(1.1f, 2.2f, 3.3f);
+    ExpectVector3Near(expected, actual, FLOAT_TOLERANCE);
 }
 
 TEST(TestVector3, DotProduct)
@@ -140,9 +140,8 @@ TEST(TestVector3, CrossProduct)
     Vector3 v1(1.1f, 2.2f, 3.3f);
     Vector3 v2(4.4f, 5.5f, 6.6f);
 
-    Vector3 v3 = v1.CrossProduct(v2);
+    Vector3 actual = v1.CrossProduct(v2);
 
-    EXPECT_NEAR(-3.63f, v3.x, FLOAT_TOLERANCE);
-    EXPECT_NEAR(7.26f, v3.y, FLOAT_TOLERANCE);
-    EXPECT_NEAR(-3.63f, v3.z, FLOAT_TOLERANCE);
+    Vector3 expected(-3.63f, 7.26f, -3.63f);
+    ExpectVector3Near(expected, actual, FLOAT_TOLERANCE);
 }
